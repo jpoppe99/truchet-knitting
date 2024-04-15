8c74013200e1864de421ff2fb5c0738b9d8e3de2
jpoppe99 <150079598+jpoppe99@users.noreply.github.com>
2024-04-15 15:13:02 -0700

    Update pattern.js

--- scripts/pattern.js
+++ scripts/pattern.js
@@ -1,7 +1,10 @@
-cellsize = 8
+
 
 function setup() {
   createCanvas(400, 400);
+  pnum = 4    //number of truchet tiles on pattern
+  psize = width/pnum;   //size of truchet tile in pixels
+  cellsize = 8 //pixels per sticth
   noFill();
   strokeWeight(1);
   stroke(0);
@@ -11,16 +14,37 @@ function setup() {
 function pattern(){
   drawborders();
   
+  for(i = 0; i < width; i += pnum){
+    for(j = 0; j < height; j += pnum){
+      tile(i,j,cellsize);
+    }
+  }
 }
 
 function drawborders(){
   for(i = 0;i < width;  i += cellsize){
     for(j = 0; j < height; j += cellsize){
-      square(i,j,cellsize);
-      
-    }
-    
+      square(i,j,cellsize); 
+    } 
   }
-  
-  
 }
+
+function tile(x,y,size){//placeholder tile function
+  fill("purple");
+  noStroke();
+  halfX = x + size/2
+  halfY = y + size/2
+  square(x,y,size);
+  stroke("green");
+  strokeWeight(size/3)
+  arc(x+size,y+size,size,size,PI,PI+HALF_PI);
+  arc(x,y,size,size,2*PI,HALF_PI);
+  
+  noFill();
+  stroke("white");
+  strokeWeight(size/20)
+  arc(x+size,y+size,size,size,PI,PI+HALF_PI);
+  arc(x,y,size,size,2*PI,HALF_PI);
+}
+  
+  
